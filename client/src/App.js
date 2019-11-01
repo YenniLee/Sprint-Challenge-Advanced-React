03d6ce7e8d5b34042262fe828592e26577de780c
Jennie Van <missjenvan@gmail.com>
2019-11-01 08:44:39 -0700

    fetch data successful

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sprint Challenge</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    playerData: []
+  }
+
+  componentDidMount() {
+    axios 
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          playerData: res.data
+        })
+      })
+      .catch(err => console.log('axios error', err))
+  }
+  render(){
+    return (
+      <div className='App'>
+        <h1>Soccer Players</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
